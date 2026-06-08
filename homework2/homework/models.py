5a3f68c7cc224a0f7c9d21c7d2567e699d70b6ba
thekrahels <krahelm@gmail.com>
2026-06-08 16:07:12 -0400

    Continued training to hit accuracy goals

--- homework2/homework/models.py
+++ homework2/homework/models.py
@@ -79,9 +79,9 @@ class MLPClassifier(nn.Module):
         ##raise NotImplementedError("MLPClassifier.__init__() is not implemented")
         c = 3 * h * w
         self.network = nn.Sequential(
-            nn.Linear(c, 128),
+            nn.Linear(c, 192),
             nn.ReLU(),
-            nn.Linear(128, num_classes),
+            nn.Linear(192, num_classes),
         )
 
 
